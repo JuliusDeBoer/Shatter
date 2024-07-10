5dafddaed2bb3324b287700f313f7e3a4b67aded
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-07-10 21:40:00 +0200

    refacor: Some other minor refactors

--- src/mask.rs
+++ src/mask.rs
@@ -7,13 +7,19 @@ use crate::manager::{StepAttributes, MASK_MANAGER, STEP_MANAGER};
 #[wasm_bindgen(js_name = showMask)]
 pub fn show_mask(mask: usize) -> usize {
     let mut binding = STEP_MANAGER.lock();
-    let manager = binding.as_mut().unwrap();
+    let manager = binding
+        .as_mut()
+        .expect("Could not get lock for step manager");
 
     manager.register(Box::from(move |attr: &mut StepAttributes| {
         let mut binding = MASK_MANAGER.lock();
-        let mask_manager = binding.as_mut().unwrap();
+        let mask_manager = binding
+            .as_mut()
+            .expect("Could not get lock for mask manager");
 
-        attr.image_buffer = mask_manager.run(&mask, attr).unwrap();
+        attr.image_buffer = mask_manager
+            .run(&mask, attr)
+            .expect("Could not create mask");
     }))
 }
 
@@ -28,13 +34,12 @@ pub fn contrast_mask(lower_bounds: f32, higher_bounds: f32) -> usize {
         let height = attr.image_buffer.height();
 
         let mut luma = attr.image_buffer.to_luma32f();
-        let pixels = luma.iter_mut().map(|p| {
-            if *p >= lower_bounds && *p <= higher_bounds {
-                return 255u8;
-            }
-
-            0u8
-        });
+        let pixels = luma
+            .iter_mut()
+            .map(|p| match *p >= lower_bounds && *p <= higher_bounds {
+                true => 255u8,
+                false => 0u8,
+            });
         image::DynamicImage::ImageLuma8(
             GrayImage::from_vec(width, height, pixels.collect())
                 .expect("Could not convert back into image. Array might not be long enough"),
